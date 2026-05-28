12aa9ae158df64ea8dbe87d29d0b22ce41825044
Yusuf Qisthi <yqisthi2002@gmail.com>
2026-05-28 19:07:15 +0700

    feat: first app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use leptos::prelude::mount_to_body;
 use leptos::prelude::*;
+
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello World!"</p> });
+    mount_to_body(App);
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button on:click=move |_| set_count.set(count.get()+1)>"Click Me!"</button>
+        <p>
+        "Count: "
+        {move || count}
+        </p>
+    }
 }
